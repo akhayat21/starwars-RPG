9d03445b1bc84c71f739acdaa77db79375452268
Akhayat <akhayat21@yahoo.com>
2019-03-02 00:07:29 -0500

    added css and html manipulation to enemy and character

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,5 @@
-var tempSelectChar;
-var tempSelectEnemy;
+var tempSelectChar = 0;
+var tempSelectEnemy = 0;
 var graveyard = [];
 
 var tempChar = {
@@ -33,115 +33,162 @@ var darthM = {
     cap: 0
 }
 
-
+main();
 // main function that calls all other functions
-function main(){
+function main() {
 
     //*****INSERT****** 
-            //reset function here: HP/AP/CAP of temps
-            //reset css/html
+    //reset function here: HP/AP/CAP of temps
+    //reset css/html
+    reset();
+
 
     //Character Selection
-    ////////////////////////////////////////////////
-
+    $(".character").click(function () {
+        if (tempSelectChar == 0) {
+            tempSelectChar = this.id;
+            console.log("you = " + tempSelectChar);
+            switch (tempSelectChar) {
+                case "obiwan":
+                    tempChar = obiwan;
+                    $("#obiwan").appendTo($("#you"));
+                    $("#luke").appendTo($("#enemy"));
+                    $("#darthM").appendTo($("#enemy"));
+                    $("#darthS").appendTo($("#enemy"));
+                    break;
+                case "luke":
+                    tempChar = luke;
+                    $("#obiwan").appendTo($("#enemy"));
+                    $("#luke").appendTo($("#you"));
+                    $("#darthM").appendTo($("#enemy"));
+                    $("#darthS").appendTo($("#enemy"));
+                    break;
+                case "darthM":
+                    tempChar = darthM;
+                    $("#obiwan").appendTo($("#enemy"));
+                    $("#luke").appendTo($("#enemy"));
+                    $("#darthM").appendTo($("#you"));
+                    $("#darthS").appendTo($("#enemy"));
+                    break;
+                case "darthS":
+                    tempChar = darthS;
+                    $("#obiwan").appendTo($("#enemy"));
+                    $("#luke").appendTo($("#enemy"));
+                    $("#darthM").appendTo($("#enemy"));
+                    $("#darthS").appendTo($("#you"));
+                    break;
+            }
+        }
+    
+    });
 
     //Enemy Selection
-    ////////////////////////////////////////////////
+    $(".character").click(function () {
+        if (tempSelectChar != 0 && tempSelectEnemy == 0 && this.id != tempSelectChar) {
+            tempSelectEnemy = this.id;
+            console.log("enemy = " + tempSelectEnemy);
+            console.log("you = " + tempSelectChar);
+            switch (tempSelectEnemy) {
+                case "obiwan":
+                    tempEnemy = obiwan;
+                    $("#obiwan").appendTo($("#defend"));
+                    break;
+                case "luke":
+                    tempEnemy = luke;
+                    $("#luke").appendTo($("#defend"));
+                    break;
+                case "darthM":
+                    tempEnemy = darthM;
+                    $("#darthM").appendTo($("#defend"));
+                    break;
+                case "darthS":
+                    tempEnemy = darthS;
+                    $("#darthS").appendTo($("#defend"));
+                    break;
+            }
+        }
+    
+    });
+
+
+
+
+    
 
 
 
     //Attacking
     ////////////////////////////////////////////////
-    
-    
+
+
 
 }
-
-
-
-
-
-
-
-
-
-// Character Selection
-$(".character").click( function(){
-    //console.log(this.id)
-    tempSelectChar = this.id;
-    //console.log(tempSelectChar);
-    switch(tempSelectChar){
-                        case "obiwan":
-                            tempChar = obiwan;
-                        //console.log(tempChar)
-                        case "luke":
-                            tempChar = luke;
-                        case "darthM":
-                            tempChar = darthM;
-                        case "darthS":
-                            tempChar = darthS;
-    }
-});
 
 //Enemy Selection
 //Move enemy to defender div
-$(".character").click( function(){
-    //console.log(this.id)
-    tempSelectEnemy = this.id;
-    //console.log(tempSelect);
-    if(tempSelectEnemy == tempSelectChar){
 
-    } else if(graveyard.indexOf(tempSelectEnemy) > -1){
-    
-    } else {
-    switch(tempSelectEnemy){
-                        case "obiwan":
-                            tempEnemy = obiwan;
-                        case "luke":
-                            tempEnemy = luke;
-                        case "darthM":
-                            tempEnemy = darthM;
-                        case "darthS":
-                            tempEnemy = darthS;
-    }
-}
-});
+
+// Character Selection
+//move all other characters to enemys
+
+
+
+
+
+
+
+
+
+
 
 
 
 //Attacking
-$("#attack").click( function(){
+$("#attack").click(function () {
 
-tempChar.hp -= tempEnemy.cap;
+    tempChar.hp -= tempEnemy.cap;
 
-tempEnemy.hp -= tempChar.ap;
+    tempEnemy.hp -= tempChar.ap;
 
-tempChar.ap *= 2;
+    tempChar.ap *= 2;
 
 });
 
 
 //Survival Check
-function SurviveCheck(){
-    
-    if(tempChar.hp == tempEnemy.hp && tempChar.hp == 0 && tempEnemy.hp ==0){
+function SurviveCheck() {
+
+    if (graveyard.length < 3 && tempChar.hp == 0 && tempEnemy.hp == 0) {
         //draw
-    } else if(tempChar.hp <= 0){
+    } else if (tempChar.hp <= 0) {
         //loss
-    } else if(tempEnemy.hp <= 0){
+    } else if (tempEnemy.hp <= 0) {
         //victory round
         //send enemy to graveyard
-        if( graveyard.length=3){
+        if (graveyard.length = 3) {
             //victory game
         } else {
             //choose next opponent
         }
     }
 
+}
 
+function reset() {
+    tempSelectChar = 0;
+    tempSelectEnemy = 0;
+    graveyard = [];
 
-
-
+    tempChar = {
+        hp: 0,
+        ap: 0,
+        cap: 0
+    }
+    tempEnemy = {
+        hp: 0,
+        ap: 0,
+        cap: 0
+    }
 }
 
 
